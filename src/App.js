16fc7204e457741cd48fc379a849c14b5f7abcf5
SwayamIITRpr <2022csb1134@iitrpr.ac.in>
2024-07-08 19:55:32 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message="This is amazing React course" />
+          <Alert message="Login to access your notes" />
           <div className="container">
             <Switch>
               <Route exact path="/">
